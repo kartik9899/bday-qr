0982ccac449ac0824153e4062b6d017c1c8fa955
kartik9899 <46781472+kartik9899@users.noreply.github.com>
2026-08-04 18:48:07 +0530

    Update redirect URL in video handler

--- api/video.js
+++ api/video.js
@@ -1,5 +1,5 @@
 export default function handler(req, res) {
   res.redirect(
-    "https://drive.google.com/file/d/YOUR_FILE_ID/view"
+    "https://drive.google.com/file/d/1IhrN2ox3BryS7vHCgLFp1CDqdVFEmWUb/view?usp=drive_link"
   );
 }
